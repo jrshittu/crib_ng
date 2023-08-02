8b2b5a371bf9fa90c908b2c3ae2b6b777764356b
Abideen <misterjrshittu@gmail.com>
2023-08-02 09:32:12 +0100

    Activated the models

--- crib_ng/crib/models.py
+++ crib_ng/crib/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Products(models.model):
+class Products(models.Model):
     product_name = models.CharField(max_length=100)
     quantity = models.IntegerField()
     date_added = models.DateTimeField("date published")
 
-class Sale(models.model):
+class Sale(models.Model):
     product_sale = models.ForeignKey(Products, on_delete=models.CASCADE)
     time_sold = models.DateTimeField('time sold')
     quantity_sold = models.IntegerField(default=0)
-    
+
 
