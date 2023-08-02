a120524a7e3d67e4a5e41e16d145ac5c5a1462e5
Abideen <misterjrshittu@gmail.com>
2023-08-02 09:14:41 +0100

    Created Models

--- crib_ng/crib/models.py
+++ crib_ng/crib/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.model):
+    product_name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    date_added = models.DateTimeField("date published")
+
+class Sale(models.model):
+    product_sale = models.IntegerField()
+    time_sold = models.DateTimeField('time sold')
+    
