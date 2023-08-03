32af1af29ef66cfa78114bfdb6ae9bf16324fd21
Abideen <misterjrshittu@gmail.com>
2023-08-03 11:38:58 +0100

    Updated views.py

--- crib_ng/crib/views.py
+++ crib_ng/crib/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render 
 from .models import Products
 
 # Create your views here.
 def index(request):
     latest_product = Products.objects.order_by("-date_added")[:5]
-    template = loader.get_template("crib/index.html")
+
     context = {
         "latest_product": latest_product,
     }
-    return HttpResponse(template.render(context, request))
+
+    return HttpResponse(render(request, "crib/index.html", context))
 
 def detail(request, product_id):
     return HttpResponse('You\'re looking at product %s' %product_id)
