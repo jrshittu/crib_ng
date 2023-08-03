4c71a9e5ad45755c6e78278f7a05161d90729722
Abideen <misterjrshittu@gmail.com>
2023-08-03 10:57:25 +0100

    Tried displaying the latest product added

--- crib_ng/crib/views.py
+++ crib_ng/crib/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Products
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello welcome to my site")
+    latest_product = Products.objects.order_by("-date_added")[:5]
+    output = ", ".join([p.product_name for p in latest_product])
+    return HttpResponse(output)
 
 def detail(request, product_id):
     return HttpResponse('You\'re looking at product %s' %product_id)
