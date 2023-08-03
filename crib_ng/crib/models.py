da0d520675b67abbf0cd92a82745912a31647805
Abideen <misterjrshittu@gmail.com>
2023-08-03 09:12:42 +0100

    Updated dp.sqlite and models.py

--- crib_ng/crib/models.py
+++ crib_ng/crib/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 class Products(models.Model):
     product_name = models.CharField(max_length=100)
     quantity = models.IntegerField(default=0)
     date_added = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.product_name
+    
+    def items_added_time(self):
+        return self.date_added >= timezone.now() - datetime.timedelta(days=1)
+    
 
 class Sale(models.Model):
     product_sale = models.ForeignKey(Products, on_delete=models.CASCADE)
